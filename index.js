26718ab2a103b37245e97685884e616dd014c7e6
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-04-10 17:37:52 -0400

    Updated

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+}
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData),
+};
+
+fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    });
+
+
+function submitData(name, email) {
+    const userData = {
+        name,
+        email
+    }
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(userData),
+    })
+    .then(resp => resp.json())
+    .then(object => document.body.innerHtml = object["id"])
+    .catch(err => document.body.innerHTML = err.message)
+}
